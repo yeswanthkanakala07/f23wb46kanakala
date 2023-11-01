a1eae63677b357bc114b8559f3601f949b7c4f84
yeswanthkanakala07 <82636668+yeswanthkanakala07@users.noreply.github.com>
2023-11-01 13:17:09 -0500

    added choose.pug

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var fruitsRouter = require('./routes/fruits');
 var gridbuildRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
 
 
 var app = express();
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/fruits', fruitsRouter);
 app.use('/board',gridbuildRouter);
+app.use('/choose',chooseRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
